2834c737100604e153e58a8b34123d96f1fb9fb1
Stefan Breunig <stefan@yrden.de>
2015-01-11 20:40:39 +0100

    include push/pulling nnev-custom branch

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git pull
+
 # manually execute hipster:update
 # RAILS_ENV=production bundle exec rake hipster:update
 git checkout master
@@ -11,6 +13,7 @@ RAILS_ENV=production ./bin/rake db:migrate
 
 git checkout nnev-custom
 git rebase master
+git push
 
 RAILS_ENV=production bundle exec rake assets:precompile
 sudo systemctl restart hipsterpizza.service
